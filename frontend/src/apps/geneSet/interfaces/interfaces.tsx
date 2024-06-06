238149e81a336598e71b543fbb3f5e0187e8e270
Emilia Ståhlbom <emilia.stahlbom@sectra.com>
2024-06-06 10:15:39 +0200

    checkout commit, started axis rescaling and gene typing

---
+++ frontend/src/apps/geneSet/interfaces/interfaces.tsx
@@ -0,0 +1,33 @@
+export interface IGene {
+    position: number,
+    name: string | undefined,
+    genome_number: number,
+    sequence_number: number,
+    sequence_id: number,
+    homology_id?: number,
+
+}
+
+export class Gene implements IGene {
+    private _position: number;
+    private _genome_number: number;
+    private _sequence_number: number;
+    private _sequence_id: number;
+    private _homology_id?: number;
+    private _name?: string;
+
+    constructor(position:number, genome_number:number, sequence_number:number, sequence_id?:number, homology_id?:number) {
+        this._position = position
+        this._genome_number = genome_number
+        this._sequence_number = sequence_number
+        this._sequence_id = sequence_id ?? sequence_number
+        this._homology_id = homology_id
+    }
+
+    public get position() {return this._position}
+    public get genome_number() {return this._genome_number}
+    public get sequence_number() {return this._sequence_number}
+    public get sequence_id() {return this._sequence_id}
+    public get homology_id() {return this._homology_id}
+    public get name() {return this._name}
+}
